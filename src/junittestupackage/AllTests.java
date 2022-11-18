c0aed85a9223183ed465d03f240f85b3507eb39a
anush <anush@10.12.14.229>
2022-11-18 23:42:31 +0530

    junittestu

---
+++ src/junittestupackage/AllTests.java
@@ -0,0 +1,11 @@
+package junittestupackage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ testaddnumber.class, testaddtwostrings.class })
+public class AllTests {
+
+}
---
+++ src/junittestupackage/junitfuncation.java
@@ -0,0 +1,5 @@
+package junittestupackage;
+
+public class junitfuncation {
+
+}
